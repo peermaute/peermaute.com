ebe2990a449369c617e5a959431ff9108b613ac4
Peer Maute <peer.maute@haw-hamburg.de>
2022-12-17 13:20:09 +0100

    music section added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,37 @@
 import styles from "./Body.module.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Body = () => {
-    return (
-        <div className={styles.body}>
-            <hr></hr>
-            <section className={styles.title}>
-                <h1>Music</h1>
-            </section>
-            <hr></hr>
-            <section className={styles.title}>
-                <h1>Cooking</h1>
-            </section>
-            <hr></hr>
-            <section className={styles.title}>
-                <h1>Projects</h1>
-            </section>
-        </div>
-    );
+  return (
+    <div className={styles.body}>
+      <hr></hr>
+      <section className={styles.sectionElement}>
+        <article className={styles.articleElement}>
+          <h1 className={styles.title}>Music</h1>
+          <p>
+            Music is something that has always played a significant role in my
+            life. I am currently a member of the chamber choir at the University
+            of Hamburg. In addition to singing, I also enjoy playing instruments
+            such as the piano and guitar. If you'd like to take a peek into my
+            more or less exquisite taste in music, you can check out my Spotify
+            playlists by clicking on the icon.
+          </p>
+        </article>
+        <a className={styles.iconLink} href="https://open.spotify.com/user/fireflies13?si=PyLH-T4iSrKnImPbaXzWHg&utm_source=copy-link">
+          <i className="bi bi-spotify" id={styles.spotifyIcon}></i>
+        </a>
+      </section>
+      <hr></hr>
+      <section className={styles.sectionElement}>
+        <h1 className={styles.title}>Cooking</h1>
+        <p></p>
+      </section>
+      <hr></hr>
+      <section className={styles.sectionElement}>
+        <h1 className={styles.title}>Projects</h1>
+      </section>
+    </div>
+  );
 };
 
 export default Body;
